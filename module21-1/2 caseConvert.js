4109136c8d34d010ed2002aeb92ad4afa199398a
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-07 08:33:23 +0000

    done

--- module21-1/2 caseConvert.js
+++ module21-1/2 caseConvert.js
@@ -1,6 +1,15 @@
 // uppercase => lowercase => uppercase
-const str = 'Hello World';
+const str = "Hello World";
 console.log(str.toLowerCase());
 
-const str2 = 'Hello World';
+const str2 = "Hello World";
 console.log(str2.toUpperCase());
+
+const userName = "blackPink";
+const userInput = "blackPinK";
+
+if (userName === userInput) {
+  console.log("equal");
+}   else {
+  console.log("not equal");
+}
