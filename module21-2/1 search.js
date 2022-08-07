73cc7e3701580ddf048d71a04ae0bb6075867c28
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-07 10:44:54 +0000

    done

--- module21-2/1 search.js
+++ module21-2/1 search.js
@@ -1,9 +1,9 @@
-const lyrics = 'tomi bondhu kala pakhi, ami jeno ki!!!bosonto kaleo tomay bolte parini....';
+const lyrics =
+  "tomi bondhu kala pakhi, ami jeno ki!!!bosonto kaleo tomay bolte parini....";
 console.log(lyrics);
 
-const searchString = 'pakhi'
-console.log(lyrics.includes(searchString))
+const searchString = "pakhi";
+console.log(lyrics.includes(searchString));
 
-
-
-
+const searchString2 = "Pakhi";
+console.log(lyrics.includes(searchString2));
