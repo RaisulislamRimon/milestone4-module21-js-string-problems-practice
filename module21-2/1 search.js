ce27b0ebe1613d84ea19b42289b6ea28285c25c7
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-07 10:29:28 +0000

    update

--- module21-2/1 search.js
+++ module21-2/1 search.js
@@ -1,3 +1,10 @@
 
-const lyrics ='tomi bondhu kala pakhi, ami jeno ki!!!bosonto kaleo tomay bolte parini....';
+const lyrics = 'tomi bondhu kala pakhi, ami jeno ki!!!bosonto kaleo tomay bolte parini....';
 console.log(lyrics);
+
+const searchString = 'pakhi'
+console.log(lyrics.includes(searchString))
+
+
+
+
